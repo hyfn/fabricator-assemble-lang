b21b4304830cf1cd0528e52a5306762b6b6b61b7
Andrew Martin <andrew@believelabs.com>
2015-09-15 12:56:00 -0700

    adding target blank

--- index.js
+++ index.js
@@ -711,7 +711,7 @@ var assemblePrototypes = function () {
         if (options.logging) {
           console.log('PROTOTYPE VIEW: ', page.view);
         }
-        indexPage += '' + '<li><a href="/flows/' + lang + '/' + flow.name + '/' + page.view + '.html">' + page.view + '</a></li>';
+        indexPage += '' + '<li><a target="_blank" href="/flows/' + lang + '/' + flow.name + '/' + page.view + '.html">' + page.view + '</a></li>';
         if (index == (flow.pages.length - 1)) {
       		indexPage += '</ol>';
         };
